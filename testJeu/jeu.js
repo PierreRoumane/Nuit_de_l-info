bcb09f7f2b5aae342d2cfc23e3641fddbfeb8c0b
Hibear <benji.lemer@gmail.com>
2022-12-02 06:02:00 +0100

    +

--- testJeu/jeu.js
+++ testJeu/jeu.js
@@ -3,6 +3,19 @@ var monTableau = [
     [0, 0]
    ];
 
-   document.write(monTableau[0][0] + " " + monTableau[0][1] +"<br/>"+ monTableau[1][0] + " " + monTableau[1][1])
+   //document.write(monTableau[0][0] + " " + monTableau[0][1] +"<br/>"+ monTableau[1][0] + " " + monTableau[1][1])
 
+var btn = document.querySelector('input');
+var txt = document.querySelector('p');
    
+btn.addEventListener('click', updateBtn);
+   
+function updateBtn() {
+    if (btn.value === 'Démarrer la machine') {
+       btn.value = 'Arrêter la machine';
+       txt.textContent = 'La machine est allumé.';
+    } else {
+       btn.value = 'Démarrer la machine';
+       txt.textContent = 'La machine est arrêtée.';
+    }
+}
